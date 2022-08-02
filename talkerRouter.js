6168d08136ad5ee6833717c74ed3405dd0a8d4b5
saraivais <isadorasaraiva95@gmail.com>
2022-08-02 16:53:27 -0300

    feat [talkerRouter]: adds get route for '/talker/:id'~ to find object in talker.json w endpoint params

--- talkerRouter.js
+++ talkerRouter.js
@@ -2,6 +2,20 @@ const express = require('express');
 const fs = require('fs/promises');
 
 const talkerRouter = express.Router();
+talkerRouter.use(express.json());
+
+talkerRouter.get('/:id', async (request, response) => {
+  const { id } = request.params;
+
+  const talkerFile = await fs.readFile('talker.json');
+  const parsedFile = await JSON.parse(talkerFile);
+
+  const chosenTalker = parsedFile.find((talker) => talker.id === Number(id));
+  if (!chosenTalker) {
+    return response.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  }
+  return response.status(200).json(chosenTalker);
+});
 
 talkerRouter.get('/', async (request, response) => {
   const talkerFile = await fs.readFile('talker.json');
@@ -13,6 +27,5 @@ talkerRouter.get('/', async (request, response) => {
 
   return response.status(200).json(getReturn);
 });
-
 
 module.exports = { talkerRouter };
