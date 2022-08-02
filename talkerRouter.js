c08f6db23c6c8aff5d3a975cfc98a2e646047ad6
saraivais <isadorasaraiva95@gmail.com>
2022-08-02 16:47:52 -0300

    feat [talkerRouter]: adds get route on '/talker'~

--- talkerRouter.js
+++ talkerRouter.js
@@ -1,6 +1,18 @@
 const express = require('express');
-// const fs = require('fs/promises');
+const fs = require('fs/promises');
 
 const talkerRouter = express.Router();
+
+talkerRouter.get('/', async (request, response) => {
+  const talkerFile = await fs.readFile('talker.json');
+  const parsedFile = await JSON.parse(talkerFile);
+
+  const getReturn = [];
+
+  parsedFile.forEach((talker) => getReturn.push(talker));
+
+  return response.status(200).json(getReturn);
+});
+
 
 module.exports = { talkerRouter };
