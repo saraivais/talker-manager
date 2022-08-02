4ba522b0ed88d0e865a6d497eeaf635ee463426d
saraivais <isadorasaraiva95@gmail.com>
2022-08-02 16:45:59 -0300

    feat [index]: implements talkerRouter~

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { talkerRouter } = require('./talkerRouter');
 
 const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
+
+// iniciando rota talker~
+app.use('/talker', talkerRouter);
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
