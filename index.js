d05f1825162c8bf491922f2b306299971510fe0f
saraivais <isadorasaraiva95@gmail.com>
2022-08-02 17:04:29 -0300

    feat [index]: imports & uses validation & error middlewares~ creates post route for '/login', uses generateToken function

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { talkerRouter } = require('./talkerRouter');
+const middlewares = require('./middlewares');
+const { errorMiddleware } = require('./errorMiddleware');
+const generateToken = require('./generateToken');
 
 const app = express();
 app.use(bodyParser.json());
@@ -11,10 +14,21 @@ const PORT = '3000';
 // iniciando rota talker~
 app.use('/talker', talkerRouter);
 
+app.post('/login',
+  middlewares.validateLoginEmail,
+  middlewares.validateLoginPassword,
+  (request, response) => {
+  // const { email, password } = request.body;
+  const randomToken = generateToken();
+  return response.status(200).json({ token: `${randomToken}` });
+});
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
+
+app.use(errorMiddleware);
 
 app.listen(PORT, () => {
   console.log('Online');
