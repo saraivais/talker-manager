0297a811a6d1dafd60f8895572fe4d5ac28c0383
saraivais <isadorasaraiva95@gmail.com>
2022-08-02 16:58:24 -0300

    feat [middlewares]: creates email & password validation middlewares~ w/ next() receiving arguments to redirect straight to error middleware

---
+++ middlewares/index.js
@@ -0,0 +1,38 @@
+function validateLoginPassword(request, response, next) {
+  const { password } = request.body;
+  const error = { status: 400 };
+
+  if (!password) {
+    error.message = 'O campo "password" é obrigatório';
+    next(error);
+  }
+  if (password.length < 6) {
+    error.message = 'O campo "password" deve ter pelo menos 6 caracteres';
+    next(error);
+  }
+  
+  next();
+}
+
+function validateLoginEmail(request, response, next) {
+  const { email } = request.body;
+  const error = { status: 400 };
+
+  if (!email) {
+    error.message = 'O campo "email" é obrigatório';
+    next(error);
+  }
+
+  const validateEmail = /\S+@\S+\.\S+/;
+  if (!validateEmail.test(email)) {
+    error.message = 'O campo "email" deve ter o formato "email@email.com"';
+    next(error);
+  }
+
+  next();
+}
+
+module.exports = { 
+  validateLoginPassword,
+  validateLoginEmail,
+};
