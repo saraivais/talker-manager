8f050a0202e1632c2c3268b2a56c2e5858c10d55
saraivais <isadorasaraiva95@gmail.com>
2022-08-02 17:58:16 -0300

    feat [talkerRouter]: creates post route for talker~ imports & uses validation middlewares for token and body data

--- talkerRouter.js
+++ talkerRouter.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const fs = require('fs/promises');
+const middlewares = require('./middlewares');
 
 const talkerRouter = express.Router();
 talkerRouter.use(express.json());
 
+// requisito 2~
 talkerRouter.get('/:id', async (request, response) => {
   const { id } = request.params;
 
@@ -17,6 +19,7 @@ talkerRouter.get('/:id', async (request, response) => {
   return response.status(200).json(chosenTalker);
 });
 
+// requisito 1~
 talkerRouter.get('/', async (request, response) => {
   const talkerFile = await fs.readFile('talker.json');
   const parsedFile = await JSON.parse(talkerFile);
@@ -26,6 +29,37 @@ talkerRouter.get('/', async (request, response) => {
   parsedFile.forEach((talker) => getReturn.push(talker));
 
   return response.status(200).json(getReturn);
+});
+
+// daqui pra baixo, faz validação dos dados que vem no body~
+talkerRouter.use(middlewares.validateToken);
+talkerRouter.use(middlewares.validateTalkerName);
+talkerRouter.use(middlewares.validateTalkerAge);
+talkerRouter.use(middlewares.validateTalkerTalk);
+talkerRouter.use(middlewares.validateTalkerRate);
+talkerRouter.use(middlewares.validateTalkerWatchedAt);
+
+// requisito 5~
+talkerRouter.post('/', 
+  async (request, response) => {
+  const { name, age, talk } = request.body;
+  const { watchedAt, rate } = talk;
+  const talkerFile = await fs.readFile('talker.json');
+  const parsedFile = await JSON.parse(talkerFile);
+  const nextId = parsedFile.length + 1;
+
+  const newTalker = {
+    name,
+    age,
+    id: nextId,
+    talk: { watchedAt, rate },
+  };
+
+  const newFileContent = [...parsedFile, newTalker];
+  const newFileContentParsed = JSON.stringify(newFileContent);
+  await fs.writeFile('talker.json', newFileContentParsed);
+
+  return response.status(201).json(newTalker);
 });
 
 module.exports = { talkerRouter };
